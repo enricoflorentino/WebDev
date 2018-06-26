d95325e260de5d89d193d11be0483f039de93959
Rico Florentino <florente@mcmaster.ca>
2018-06-26 13:21:24 -0400

    jQuery basics

---
+++ jQuery-basics/test.js
@@ -0,0 +1,11 @@
+$("div").css("backgroundColor", "purple");
+
+$(".highlight").css("width","200px");
+
+$("#change").text("Orange");
+
+$("#addlink").html("<a href='https://google.com'>click to reroute to google</a>");
+
+$("#correct").addClass("correct");
+$("#done").addClass("done");
+$("#wrong").addClass("wrong");
