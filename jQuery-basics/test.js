edaefa3d34e351038fb38706280aaf70543f2def
Rico Florentino <florente@mcmaster.ca>
2018-06-27 01:12:53 -0400

    finished basic jQuery

--- jQuery-basics/test.js
+++ jQuery-basics/test.js
@@ -9,3 +9,26 @@ $("#addlink").html("<a href='https://google.com'>click to reroute to google</a>"
 $("#correct").addClass("correct");
 $("#done").addClass("done");
 $("#wrong").addClass("wrong");
+
+
+$("#alerty").click(function(){
+	alert("You have clicked this button");
+});
+
+$("#encrypt").keypress(function(event){
+	if(event.which === 13)
+	{
+		alert("f1yd" + $(this).val() + "ztfgh");
+	}
+});
+
+
+$("#hover1").on("mouseenter", function(){
+	$(this).css("backgroundColor", "purple");
+});
+
+
+
+$("#fadethem").on("click",function(){
+	$("div").fadeOut(700);
+});
