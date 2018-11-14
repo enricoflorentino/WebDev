da5218831d6be279224626a2e1d5c383e6f7c678
Rico Florentino <florente@mcmaster.ca>
2018-11-14 15:58:37 -0500

    angular

---
+++ angular-learn/typescript-start/main.ts
@@ -0,0 +1,13 @@
+let funct1 = () => console.log("no params");
+
+let funct2 = (printstring) => console.log(printstring);
+
+let funct3 = (a,b) => console.log(a*b);
+
+funct1();
+funct2("1 param");
+funct3(2,3);
+
+let a: number;
+let b: string;
+let c: boolean;
