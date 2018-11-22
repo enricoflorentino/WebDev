3cbf1b7be5c22a32073cb2938383e671aafe0995
Rico Florentino <florente@mcmaster.ca>
2018-11-21 23:30:54 -0500

    progress

--- angular-learn/hello-world/src/app/course/course.component.ts
+++ angular-learn/hello-world/src/app/course/course.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
-
+  title = "List of courses";
+  imageUrl = "https://images.unsplash.com/photo-1542773049-ccc00e2a69d1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9b6b937eb302431329b198824f307635&auto=format&fit=crop&w=1500&q=80";
   constructor() { }
 
   ngOnInit() {
