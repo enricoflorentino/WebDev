8bf3acafa05e09e601a4db7cb202555ff1bc9bed
ricoflorentino <enrico.f1998@gmail.com>
2019-08-19 21:36:57 -0400

    first vue cli project

--- vue-learn/vue-basics/app.js
+++ vue-learn/vue-basics/app.js
@@ -8,7 +8,13 @@ new Vue({
         wage: 10,
         favColor: 'blue',
         find: "me",
-        showName: true
+        showName: true,
+        fruits: ["banana", "apple", "orange", "strawberry"],
+        employees: [
+            {name: "Ryan", age: 23, job: "Soft Dev"},
+            {name: "Tim", age: 13, job: "Data"},
+            {name: "John", age: 19, job: "QA"}
+        ]
     },
     methods: {
         greet(time){
---
+++ vue-learn/vue-cli-project/src/components/Test.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="test">
+        <h2>{{ test1 }}</h2>
+        <h6>{{ secretmsg }}</h6>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Test',
+    data () {
+        return {
+            test1: "From Test",
+            secretmsg: "This is from a testing component"
+        }
+    }
+}
+</script>
+
+
+<style>
+.test {
+    border: 1px solid red;
+}
+</style>
